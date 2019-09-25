72a6f91db77c9c612c48be60157b6bdc071bd297
root <root@localhost.localdomain>
2019-09-25 19:01:21 +0530

    changed commands

--- http.sh
+++ http.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 sudo yum -y install httpd;
-sudo firewall-cmd --add-port=80/tcp --add-service=http --permanent;
+sudo firewall-cmd --add-port=80/tcp --permanent;
+sudo firewall-cmd --add-service=http --permanent;
+sudo firewall-cmd --reload;
+sudo systemctl start httpd;
+sudo systemctl enable httpd;
+
 
